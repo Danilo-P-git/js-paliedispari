ab0e05c86696ae15e4602ee0e88c09f6b3866193
Danilo Patanè <d.patane98@gmail.com>
2020-08-27 16:04:31 +0200

    Palindroma done

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,6 +1,14 @@
+// var parolaInserita = prompt("Inserisci una parola");
 function palindroma(Parola1) {
   var parolaPalindroma = parolaInserita.split('').reverse().join(''); ;
-  return parolaPalindroma === parolaInserita;
+  return parolaPalindroma
 }
 var parolaInserita = prompt("Inserisci una parola");
+
 var parolaCheck = palindroma(parolaInserita);
+if (parolaInserita == parolaCheck) {
+  console.log("La parola che hai inserito è palindorma");
+}
+else {
+  console.log("La parola che hai inserito non è palindroma");
+}
