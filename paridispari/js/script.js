87ff978d986ce14915274fcb2081532c5ce00432
Danilo Patanè <d.patane98@gmail.com>
2020-08-27 16:55:44 +0200

    Pari dispari Wip

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -1,4 +1,4 @@
-var pariODispari = prompt("Scegli pari o dispari");
+var pariODispari = prompt("Pari o dispari");
 var numeroScelto = parseInt(prompt("Inserisci un numero tra 1 e 5"));
 function genNumero() {
   var numeroGenerato = Math.floor(Math.random() * 5) + 1;
@@ -6,4 +6,23 @@ function genNumero() {
 }
 
 var totale = numeroScelto + genNumero();
-console.log(totale);
+console.log("il numero è " + totale);
+
+function checkPariODispari() {
+ var check = false;
+ if (totale % 2 == 0) {
+  return  check = true;
+ }
+ else {
+   return check = false;
+ }
+}
+
+var checkTest = checkPariODispari(totale);
+
+if (checkTest == true) {
+  console.log("Il numero è pari se hai scelto pari hai vinto.")
+}
+else {
+  console.log("il numero è dispari se hai scelto dispari hai vinto");
+}
