9ae5f8fa02235f6778cea36df070f81d4e723b07
Danilo Patanè <d.patane98@gmail.com>
2020-08-28 10:05:03 +0200

    creo i commenti

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,11 +1,13 @@
-// var parolaInserita = prompt("Inserisci una parola");
+// faccio la funzione per girare una parola
 function palindroma(Parola1) {
   var parolaPalindroma = parolaInserita.split('').reverse().join(''); ;
   return parolaPalindroma
 }
+// inserisco una parola
 var parolaInserita = prompt("Inserisci una parola");
-
+// dichiaro una variabile di appoggio con la funzione
 var parolaCheck = palindroma(parolaInserita);
+// creo un if e stampo
 if (parolaInserita == parolaCheck) {
   console.log("La parola che hai inserito è palindorma");
 }
